df4b6c4a64991cb8aba9174b85ded7ac2644368d
Apatpoh <apatpoh96@gmail.com>
2017-03-10 16:06:04 +0200

    calculator 1

---
+++ Lab2/Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.text_box = new System.Windows.Forms.Label();
+            this.button_7 = new System.Windows.Forms.PictureBox();
+            this.button_8 = new System.Windows.Forms.PictureBox();
+            this.button_9 = new System.Windows.Forms.PictureBox();
+            this.button_4 = new System.Windows.Forms.PictureBox();
+            this.button_5 = new System.Windows.Forms.PictureBox();
+            this.button_6 = new System.Windows.Forms.PictureBox();
+            this.button_3 = new System.Windows.Forms.PictureBox();
+            this.button_2 = new System.Windows.Forms.PictureBox();
+            this.button_1 = new System.Windows.Forms.PictureBox();
+            this.button_0 = new System.Windows.Forms.PictureBox();
+            this.button_ch_sg = new System.Windows.Forms.PictureBox();
+            this.button_dot = new System.Windows.Forms.PictureBox();
+            this.button_imp = new System.Windows.Forms.PictureBox();
+            this.button_pw = new System.Windows.Forms.PictureBox();
+            this.button_inm = new System.Windows.Forms.PictureBox();
+            this.button_min = new System.Windows.Forms.PictureBox();
+            this.button_add = new System.Windows.Forms.PictureBox();
+            this.button_rad = new System.Windows.Forms.PictureBox();
+            this.button_egal = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.button_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_ch_sg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_dot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_imp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_pw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_inm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_min)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_add)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_rad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_egal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // text_box
+            // 
+            this.text_box.BackColor = System.Drawing.Color.Transparent;
+            this.text_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text_box.Location = new System.Drawing.Point(144, 88);
+            this.text_box.Name = "text_box";
+            this.text_box.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.text_box.Size = new System.Drawing.Size(587, 69);
+            this.text_box.TabIndex = 2;
+            // 
+            // button_7
+            // 
+            this.button_7.BackColor = System.Drawing.Color.Transparent;
+            this.button_7.Location = new System.Drawing.Point(56, 238);
+            this.button_7.Name = "button_7";
+            this.button_7.Size = new System.Drawing.Size(141, 105);
+            this.button_7.TabIndex = 4;
+            this.button_7.TabStop = false;
+            // 
+            // button_8
+            // 
+            this.button_8.BackColor = System.Drawing.Color.Transparent;
+            this.button_8.Location = new System.Drawing.Point(213, 238);
+            this.button_8.Name = "button_8";
+            this.button_8.Size = new System.Drawing.Size(141, 105);
+            this.button_8.TabIndex = 5;
+            this.button_8.TabStop = false;
+            // 
+            // button_9
+            // 
+            this.button_9.BackColor = System.Drawing.Color.Transparent;
+            this.button_9.Location = new System.Drawing.Point(370, 239);
+            this.button_9.Name = "button_9";
+            this.button_9.Size = new System.Drawing.Size(141, 105);
+            this.button_9.TabIndex = 6;
+            this.button_9.TabStop = false;
+            // 
+            // button_4
+            // 
+            this.button_4.BackColor = System.Drawing.Color.Transparent;
+            this.button_4.Location = new System.Drawing.Point(56, 354);
+            this.button_4.Name = "button_4";
+            this.button_4.Size = new System.Drawing.Size(141, 105);
+            this.button_4.TabIndex = 7;
+            this.button_4.TabStop = false;
+            // 
+            // button_5
+            // 
+            this.button_5.BackColor = System.Drawing.Color.Transparent;
+            this.button_5.Location = new System.Drawing.Point(213, 354);
+            this.button_5.Name = "button_5";
+            this.button_5.Size = new System.Drawing.Size(141, 105);
+            this.button_5.TabIndex = 8;
+            this.button_5.TabStop = false;
+            // 
+            // button_6
+            // 
+            this.button_6.BackColor = System.Drawing.Color.Transparent;
+            this.button_6.Location = new System.Drawing.Point(370, 354);
+            this.button_6.Name = "button_6";
+            this.button_6.Size = new System.Drawing.Size(141, 105);
+            this.button_6.TabIndex = 9;
+            this.button_6.TabStop = false;
+            // 
+            // button_3
+            // 
+            this.button_3.BackColor = System.Drawing.Color.Transparent;
+            this.button_3.Location = new System.Drawing.Point(370, 468);
+            this.button_3.Name = "button_3";
+            this.button_3.Size = new System.Drawing.Size(141, 105);
+            this.button_3.TabIndex = 10;
+            this.button_3.TabStop = false;
+            // 
+            // button_2
+            // 
+            this.button_2.BackColor = System.Drawing.Color.Transparent;
+            this.button_2.Location = new System.Drawing.Point(213, 469);
+            this.button_2.Name = "button_2";
+            this.button_2.Size = new System.Drawing.Size(141, 105);
+            this.button_2.TabIndex = 11;
+            this.button_2.TabStop = false;
+            // 
+            // button_1
+            // 
+            this.button_1.BackColor = System.Drawing.Color.Transparent;
+            this.button_1.Location = new System.Drawing.Point(56, 469);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(141, 105);
+            this.button_1.TabIndex = 12;
+            this.button_1.TabStop = false;
+            // 
+            // button_0
+            // 
+            this.button_0.BackColor = System.Drawing.Color.Transparent;
+            this.button_0.Location = new System.Drawing.Point(56, 583);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(141, 105);
+            this.button_0.TabIndex = 13;
+            this.button_0.TabStop = false;
+            // 
+            // button_ch_sg
+            // 
+            this.button_ch_sg.BackColor = System.Drawing.Color.Transparent;
+            this.button_ch_sg.Location = new System.Drawing.Point(213, 583);
+            this.button_ch_sg.Name = "button_ch_sg";
+            this.button_ch_sg.Size = new System.Drawing.Size(141, 105);
+            this.button_ch_sg.TabIndex = 14;
+            this.button_ch_sg.TabStop = false;
+            // 
+            // button_dot
+            // 
+            this.button_dot.BackColor = System.Drawing.Color.Transparent;
+            this.button_dot.Location = new System.Drawing.Point(370, 583);
+            this.button_dot.Name = "button_dot";
+            this.button_dot.Size = new System.Drawing.Size(141, 105);
+            this.button_dot.TabIndex = 15;
+            this.button_dot.TabStop = false;
+            // 
+            // button_imp
+            // 
+            this.button_imp.BackColor = System.Drawing.Color.Transparent;
+            this.button_imp.Location = new System.Drawing.Point(527, 238);
+            this.button_imp.Name = "button_imp";
+            this.button_imp.Size = new System.Drawing.Size(141, 105);
+            this.button_imp.TabIndex = 16;
+            this.button_imp.TabStop = false;
+            // 
+            // button_pw
+            // 
+            this.button_pw.BackColor = System.Drawing.Color.Transparent;
+            this.button_pw.Location = new System.Drawing.Point(682, 239);
+            this.button_pw.Name = "button_pw";
+            this.button_pw.Size = new System.Drawing.Size(141, 105);
+            this.button_pw.TabIndex = 17;
+            this.button_pw.TabStop = false;
+            // 
+            // button_inm
+            // 
+            this.button_inm.BackColor = System.Drawing.Color.Transparent;
+            this.button_inm.Location = new System.Drawing.Point(527, 354);
+            this.button_inm.Name = "button_inm";
+            this.button_inm.Size = new System.Drawing.Size(141, 105);
+            this.button_inm.TabIndex = 18;
+            this.button_inm.TabStop = false;
+            // 
+            // button_min
+            // 
+            this.button_min.BackColor = System.Drawing.Color.Transparent;
+            this.button_min.Location = new System.Drawing.Point(527, 469);
+            this.button_min.Name = "button_min";
+            this.button_min.Size = new System.Drawing.Size(141, 105);
+            this.button_min.TabIndex = 19;
+            this.button_min.TabStop = false;
+            // 
+            // button_add
+            // 
+            this.button_add.BackColor = System.Drawing.Color.Transparent;
+            this.button_add.Location = new System.Drawing.Point(527, 583);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(141, 105);
+            this.button_add.TabIndex = 20;
+            this.button_add.TabStop = false;
+            // 
+            // button_rad
+            // 
+            this.button_rad.BackColor = System.Drawing.Color.Transparent;
+            this.button_rad.Location = new System.Drawing.Point(682, 354);
+            this.button_rad.Name = "button_rad";
+            this.button_rad.Size = new System.Drawing.Size(141, 105);
+            this.button_rad.TabIndex = 21;
+            this.button_rad.TabStop = false;
+            // 
+            // button_egal
+            // 
+            this.button_egal.BackColor = System.Drawing.Color.Transparent;
+            this.button_egal.Location = new System.Drawing.Point(682, 469);
+            this.button_egal.Name = "button_egal";
+            this.button_egal.Size = new System.Drawing.Size(141, 219);
+            this.button_egal.TabIndex = 22;
+            this.button_egal.TabStop = false;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(882, 753);
+            this.Controls.Add(this.button_egal);
+            this.Controls.Add(this.button_rad);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_min);
+            this.Controls.Add(this.button_inm);
+            this.Controls.Add(this.button_pw);
+            this.Controls.Add(this.button_imp);
+            this.Controls.Add(this.button_dot);
+            this.Controls.Add(this.button_ch_sg);
+            this.Controls.Add(this.button_0);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_3);
+            this.Controls.Add(this.button_6);
+            this.Controls.Add(this.button_5);
+            this.Controls.Add(this.button_4);
+            this.Controls.Add(this.button_9);
+            this.Controls.Add(this.button_8);
+            this.Controls.Add(this.button_7);
+            this.Controls.Add(this.text_box);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.button_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_ch_sg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_dot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_imp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_pw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_inm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_min)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_add)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_rad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_egal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text_box;
+        private System.Windows.Forms.PictureBox button_7;
+        private System.Windows.Forms.PictureBox button_8;
+        private System.Windows.Forms.PictureBox button_9;
+        private System.Windows.Forms.PictureBox button_4;
+        private System.Windows.Forms.PictureBox button_5;
+        private System.Windows.Forms.PictureBox button_6;
+        private System.Windows.Forms.PictureBox button_3;
+        private System.Windows.Forms.PictureBox button_2;
+        private System.Windows.Forms.PictureBox button_1;
+        private System.Windows.Forms.PictureBox button_0;
+        private System.Windows.Forms.PictureBox button_ch_sg;
+        private System.Windows.Forms.PictureBox button_dot;
+        private System.Windows.Forms.PictureBox button_imp;
+        private System.Windows.Forms.PictureBox button_pw;
+        private System.Windows.Forms.PictureBox button_inm;
+        private System.Windows.Forms.PictureBox button_min;
+        private System.Windows.Forms.PictureBox button_add;
+        private System.Windows.Forms.PictureBox button_rad;
+        private System.Windows.Forms.PictureBox button_egal;
+    }
+}
+
