8a3fb77eab2106f5ba3473abaac06f4b330524d9
Apatpoh <apatpoh96@gmail.com>
2017-03-27 12:00:58 +0300

    Raport

--- Lab2/Calculator/Calculator/Form1.cs
+++ Lab2/Calculator/Calculator/Form1.cs
@@ -20,6 +20,201 @@ namespace Calculator
         public Calculator()
         {
             InitializeComponent();
+            this.KeyDown += Calculator_KeyDown;
+        }
+
+        private void Calculator_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.NumPad0 || e.KeyCode == Keys.D0)
+            {
+                text_box.Text += "0";
+            }
+
+            if (e.KeyCode == Keys.NumPad1 || e.KeyCode == Keys.D1)
+            {
+                text_box.Text += "1";
+            }
+
+            if (e.KeyCode == Keys.NumPad2 || e.KeyCode == Keys.D2)
+            {
+                text_box.Text += "2";
+            }
+
+            if (e.KeyCode == Keys.NumPad3 || e.KeyCode == Keys.D3)
+            {
+                text_box.Text += "3";
+            }
+
+            if (e.KeyCode == Keys.NumPad4 || e.KeyCode == Keys.D4)
+            {
+                text_box.Text += "4";
+            }
+
+            if (e.KeyCode == Keys.NumPad5 || e.KeyCode == Keys.D5)
+            {
+                text_box.Text += "5";
+            }
+
+            if (e.KeyCode == Keys.NumPad6 || e.KeyCode == Keys.D6)
+            {
+                text_box.Text += "6";
+            }
+
+            if (e.KeyCode == Keys.NumPad7 || e.KeyCode == Keys.D7)
+            {
+                text_box.Text += "7";
+            }
+
+            if (e.KeyCode == Keys.NumPad8 || e.KeyCode == Keys.D8)
+            {
+                text_box.Text += "8";
+            }
+
+            if (e.KeyCode == Keys.NumPad9 || e.KeyCode == Keys.D9)
+            {
+                text_box.Text += "9";
+            }
+
+            if (e.KeyCode == Keys.Multiply)
+            {
+                if (text_box.Text == string.Empty)
+                {
+                    text_box.Text = "0";
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "x";
+                    sign = 3;
+                }
+                else
+                {
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "x";
+                    sign = 3;
+                }
+          
+        }
+
+            if (e.KeyCode == Keys.Divide)
+            {
+                if (text_box.Text == string.Empty)
+                {
+                    text_box.Text = "0";
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "/";
+                    sign = 4;
+                }
+                else
+                {
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "/";
+                    sign = 4;
+                }
+            }
+
+            if (e.KeyCode == Keys.Add)
+            {
+                if (text_box.Text == string.Empty)
+                {
+                    text_box.Text = "0";
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "+";
+                    sign = 1;
+                }
+                else
+                {
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "+";
+                    sign = 1;
+                }
+            }
+
+            if (e.KeyCode == Keys.Subtract)
+            {
+                if (text_box.Text == string.Empty)
+                {
+                    text_box.Text = "0";
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "-";
+                    sign = 2;
+                }
+                else
+                {
+                    n1 = text_box.Text;
+                    text_box.Text = "";
+                    text_box2.Text = "-";
+                    sign = 2;
+                }
+            }
+
+            if (e.KeyCode == Keys.Decimal)
+            {
+                text_box2.Text += "*";
+            }
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (text_box.Text == string.Empty)
+                {
+                    text_box.Text = "0";
+                }
+                n2 = text_box.Text;
+                text_box.Text = "";
+                a = Convert.ToDouble(n1);
+                b = Convert.ToDouble(n2);
+                text_box2.Text = "=";
+                switch (sign)
+                {
+                    case 1:
+                        c = a + b;
+                        text_box.Text = Convert.ToString(c);
+                        break;
+                    case 2:
+                        c = a - b;
+                        text_box.Text = Convert.ToString(c);
+                        break;
+                    case 3:
+                        c = a * b;
+                        text_box.Text = Convert.ToString(c);
+                        break;
+                    case 4:
+                        c = a / b;
+                        text_box.Text = Convert.ToString(c);
+                        break;
+                    case 5:
+                        int i = 1;
+                        c = a;
+                        while (i < b)
+                        {
+                            c = c * a;
+                            i++;
+                        }
+                        text_box.Text = Convert.ToString(c);
+                        break;
+                    case 6:
+                        c = Math.Pow(a, 1 / b);
+                        text_box.Text = Convert.ToString(c);
+                        break;
+                }
+            }
+
+            if (e.KeyCode == Keys.Back && text_box.Text != string.Empty)
+            {
+                text_box.Text = text_box.Text.Remove(text_box.Text.Length - 1);
+                if (text_box2.Text != string.Empty)
+                    text_box2.Text = string.Empty;
+            }
+
+            if (e.KeyCode == Keys.Delete)
+            {
+                text_box.Text = "";
+                text_box2.Text = "";
+            }
         }
 
         private void button_1_Click(object sender, EventArgs e)
@@ -83,7 +278,15 @@ namespace Calculator
 
         private void button_dot_Click(object sender, EventArgs e)
         {
-                text_box.Text += ".";
+            if (text_box.Text == string.Empty)
+            {
+                text_box.Text = "0";
+                n1 = text_box.Text;
+            }
+            if (!text_box.Text.Contains("."))
+                {
+                    text_box.Text += ".";
+                }
         }
 
         private void button_imp_Click(object sender, EventArgs e)
